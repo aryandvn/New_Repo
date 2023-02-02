2f1f2f764980b0775c04011d653cfbf8e08e4e86
aryandvn <54626547+aryandvn@users.noreply.github.com>
2023-02-01 21:36:33 -0800

    Update main.cs

---
+++ main.cs
@@ -0,0 +1,11 @@
+//assigns default value 01/01/0001 00:00:00
+DateTime dt1 = new DateTime(); 
+
+//assigns year, month, day
+DateTime dt2 = new DateTime(2015, 12, 31); 
+
+//assigns year, month, day, hour, min, seconds
+DateTime dt3 = new DateTime(2015, 12, 31, 5, 10, 20);
+ 
+//assigns year, month, day, hour, min, seconds, UTC timezone
+DateTime dt4 = new DateTime(2015, 12, 31, 5, 10, 20, DateTimeKind.Utc);
